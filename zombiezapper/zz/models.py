a2a7d8f923274464d9ef9b1cc4548f0652f5e66a
kimx5230 <kimx5230@umn.edu>
2017-04-23 11:15:34 -0500

    changed project name

--- zombiezapper/zz/models.py
+++ zombiezapper/zz/models.py
@@ -9,16 +9,18 @@ class User(models.Model):
 	password = models.CharField(max_length = 30)
 	statement = models.CharField(max_length = 150)
 	level = models.IntegerField()
-	commitments = ForeignKey('sites.Habit')
+#	commitments = models.ForeignKey('sites.Habit')
 
 	def __str__(self):
 		return user
 
 class Habit(models.Model):
-	author = ForeignKey('auth.User')
+	author = models.ForeignKey('auth.User')
 	trigger = models.CharField(max_length = 500)
 	habit = models.CharField(max_length = 100)
 	num_commitments = models.IntegerField()
+	publish_date = models.DateTimeField(
+		blank = True, null = True)
 #	comments = 
 
 	def publish(self):
