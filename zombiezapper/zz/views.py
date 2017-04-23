372e6ef2b77feff41683b08b68959cd61d1517ff
David Munkvold <dmunkvold@gmail.com>
2017-04-23 11:27:59 -0500

    homepage routing works yay

--- zombiezapper/zz/views.py
+++ zombiezapper/zz/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'site/home_page.html')
+    return render(request, 'zz/home_page.html')
